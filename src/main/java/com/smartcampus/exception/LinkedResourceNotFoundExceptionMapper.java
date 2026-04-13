1830c59d182becc459fd27c3ec35acf003eb8bb0
GovinduDandeniya <govindudandeniya@gmail.com>
2026-04-13 14:46:13 +0530

    Add ExceptionMapper for LinkedResourceNotFoundException returning 422

---
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,24 @@
+package com.smartcampus.exception;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException exception) {
+        Map<String, Object> error = new LinkedHashMap<>();
+        error.put("status", 422);
+        error.put("error", "Unprocessable Entity");
+        error.put("message", exception.getMessage());
+        return Response.status(422)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
