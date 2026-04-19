29307f8e2a2b49118c80283d3daacba5cf937c3f
GovinduDandeniya <govindudandeniya@gmail.com>
2026-04-19 14:59:29 +0530

    Fix Jackson JSON provider registration in ResourceConfig

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -2,6 +2,7 @@ package com.smartcampus;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.net.URI;
@@ -17,7 +18,8 @@ public class Main {
         final ResourceConfig config = new ResourceConfig()
                 .packages("com.smartcampus.resource",
                           "com.smartcampus.exception",
-                          "com.smartcampus.filter");
+                          "com.smartcampus.filter")
+                .register(JacksonFeature.class);
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
     }
 
